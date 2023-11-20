de564d692d73be270e4bcca8bfd867ca8c573cbc
Daniel-max640 <dan.msaj@gmail.com>
2023-11-20 09:32:05 -0500

    ull

--- src/app.js
+++ src/app.js
@@ -1,19 +1,31 @@
-const express = require('express');
-const helmet = require('helmet');
-const cors = require('cors');
-require('dotenv').config();
-const router = require('./routes');
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+require("dotenv").config();
+const router = require("./routes");
+const handlebars = require("express-handlebars");
 
 // Esta es nuestra aplicación
 const app = express();
 
-// Middlewares 
+// Middlewares
 app.use(express.json());
-app.use(helmet({
+app.use(
+  helmet({
     crossOriginResourcePolicy: false,
-}));
+  })
+);
 app.use(cors());
+app.set("view engine", "handlebars");
+app.set("views", __dirname + "/views");
+app.engine(
+  "handlebars",
+  handlebars.engine({
+    layoutsDir: __dirname + "/views/layouts",
+    defaultLayout: "index",
+  })
+);
 
-app.use('/', router);
+app.use("/", router); //hacemos referencia a un router, donde se trabajara
 
 module.exports = app;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,31 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 // colocar las rutas aquí
-router.get('/', (req, res) => {
-    res.send("Welcome to express");
+router.get("/", (req, res) => {
+  res.render("home");
 });
 
+router.get("/projects", (req, res) => {
+  res.render("projects");
+});
+
+router.get("/team", (req, res) => {
+  res.render("team");
+});
+
+router.get("/team/:name", (req, res) => {
+  const { name } = req.params;
+  res.render("team-member", { name });
+});
+
+router.get("/projects/:id", (req, res) => {
+  const { id } = req.params;
+  res.render("projects", { id });
+});
+
+router.get("/about-me", (req, res) => {
+  res.render("about-me");
+});
 
 module.exports = router;
